2f7e8906f60323f1ac7fc7083f88f46ef1f1b24a
crazyboomegg <crazyboomegg@gmail.com>
2020-11-20 17:03:06 +0800

    cell size

--- APICloud/PhotoCollectionViewController.swift
+++ APICloud/PhotoCollectionViewController.swift
@@ -12,12 +12,43 @@ import UIKit
 class PhotoCollectionViewController: UICollectionViewController {
     
     var photos = [Photo]()
+    @IBOutlet var myCollectionView : UICollectionView!
+  
+        var screenSize: CGRect!
+        var screenWidth: CGFloat!
+        var screenHeight: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("collectionview 讀取成功")
         
+        screenSize = UIScreen.main.bounds
+                screenWidth = screenSize.width
+                screenHeight = screenSize.height
+    
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+               layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+               layout.itemSize = CGSize(width: screenWidth/3, height: screenWidth/3)
+               layout.minimumInteritemSpacing = 0
+               layout.minimumLineSpacing = 0
+              myCollectionView!.collectionViewLayout = layout
+        
+      /*
+        let layout = self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout
+        // let width = (view.bounds.width - 100) / 2
+        
+        layout?.sectionInset = UIEdgeInsets(top: -1, left: -1, bottom: -1, right: -1) // section與section之間的距離(如果只有一個section，可以想像成frame)
+        layout?.itemSize = CGSize(width: view.bounds.width/4, height: view.bounds.width/4) // cell的寬、高
+        layout?.scrollDirection = UICollectionView.ScrollDirection.vertical
 
+        layout?.minimumLineSpacing = 0;
+        layout?.minimumInteritemSpacing = 0;
+ */
+        
+        
         
         fetchJson()
     }
@@ -78,6 +109,12 @@ class PhotoCollectionViewController: UICollectionViewController {
         }
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let sideSize = (traitCollection.horizontalSizeClass == .compact && traitCollection.verticalSizeClass == .regular) ? 80.0 : 128.0
+        return CGSize(width: sideSize, height: sideSize)
     }
     
 }
